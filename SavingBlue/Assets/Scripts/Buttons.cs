8194bd172d7d4063e45d68f543bff3a949701803
Ioana <Ioananicoleta.Parosu.6221@student.uu.se>
2020-05-12 21:07:56 +0200

    Added trasnition between scenes

--- SavingBlue/Assets/Scripts/Buttons.cs
+++ SavingBlue/Assets/Scripts/Buttons.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class Buttons : MonoBehaviour
 {
+    bool changing = false;
+    public float delay = 3;
+
+    public GameObject fade;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,16 +18,9 @@ public class Buttons : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E) && changing == false)
         {
-            if(SceneManager.GetActiveScene().name == "YouLose" || SceneManager.GetActiveScene().name == "CreditsScene")
-            {
-                Menu();
-            }
-            else
-            {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-            }
+            StartCoroutine(ChangeScene());
         }
 
     }
@@ -52,5 +49,22 @@ public class Buttons : MonoBehaviour
     public void WinScene()
     {
         SceneManager.LoadScene("YouWin");
+    }
+
+    IEnumerator ChangeScene()
+    {
+        changing = true;
+
+        Instantiate(fade, Vector3.zero, Quaternion.identity);
+
+        yield return new WaitForSeconds(delay);
+        if (SceneManager.GetActiveScene().name == "YouLose" || SceneManager.GetActiveScene().name == "CreditsScene")
+        {
+            Menu();
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
     }
 }

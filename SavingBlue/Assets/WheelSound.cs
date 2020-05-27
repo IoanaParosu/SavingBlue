e28979743d84ea9362a51664c981ef4d4b7c7c7e
dan1mk <60133884+dan1mk@users.noreply.github.com>
2020-05-27 17:15:57 +0200

    wheel sound

---
+++ SavingBlue/Assets/WheelSound.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelSound : MonoBehaviour
+{
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(rb.angularVelocity == 0)
+        {
+            AudioManager.instance.Stop("Wheel");
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Wheel")
+        {
+            if(rb.angularVelocity != 0)
+            {
+                AudioManager.instance.Play("Wheel");
+            }
+        }
+    }
+}

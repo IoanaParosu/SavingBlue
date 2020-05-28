54a488a0897c402d92fa196bd68bda7b35a25a66
dan1mk <60133884+dan1mk@users.noreply.github.com>
2020-05-28 11:34:40 +0200

    Update NetScript.cs

--- SavingBlue/Assets/Scripts/NetScript.cs
+++ SavingBlue/Assets/Scripts/NetScript.cs
@@ -12,13 +12,14 @@ public class NetScript : MonoBehaviour
     public FishMouth fishMouth;
     public Transform fishPos;
     float distance;
+    float volume = 0.2f;
    
     void Start()
     {
         NetSpeed = 1.0f;
         AccelerationTime = 5.0f;
-        FindObjectOfType<AudioManager>().Play("FishNet");
-        FindObjectOfType<AudioManager>().ChangeVolume("FishNet", 0.2f);
+        AudioManager.instance.Play("FishNet");
+        AudioManager.instance.ChangeVolume("FishNet", volume);
     }
 
    
@@ -40,20 +41,31 @@ public class NetScript : MonoBehaviour
         {
             distance = fishPos.transform.position.y - transform.position.y;
         }
-        
 
-        if(distance < 20 && distance > 10)
+        AudioManager.instance.ChangeVolume("FishNet", volume);
+        Debug.Log("volume: " + volume);
+
+
+        if (distance < 15 && volume < 1)
         {
-            FindObjectOfType<AudioManager>().ChangeVolume("FishNet", 0.5f);
+            volume += 0.001f;
         }
-        else if(distance < 10)
+        else if(distance > 15 && volume > 0.001f)
         {
-            FindObjectOfType<AudioManager>().ChangeVolume("FishNet", 1f);
+            volume -= 0.001f;
         }
-        else if(distance > 30)
-        {
-            FindObjectOfType<AudioManager>().Stop("FishNet");
-        }
+        //if(distance < 20 && distance > 10)
+        //{
+        //    FindObjectOfType<AudioManager>().ChangeVolume("FishNet", 0.5f);
+        //}
+        //else if(distance < 10)
+        //{
+        //    FindObjectOfType<AudioManager>().ChangeVolume("FishNet", 1f);
+        //}
+        //else if(distance > 30)
+        //{
+        //    FindObjectOfType<AudioManager>().Stop("FishNet");
+        //}
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
